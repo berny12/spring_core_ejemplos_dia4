87cdb4fcbb39de1f889d9d1aab6caba0e16eb948
edgar bernal remoto <tecualoyaster@gmail.com>
2015-08-15 10:55:06 -0500

    advisors
    emplear apecto con xml, pero versiones 2.0 no implementar en vida real

--- labs-modulo3/src/test/java/com/synergyj/cursos/spring/pruebas/AopXMLTestCase.java
+++ labs-modulo3/src/test/java/com/synergyj/cursos/spring/pruebas/AopXMLTestCase.java
@@ -14,6 +14,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.synergyj.cursos.spring.service.TarjetaCreditoService;
@@ -24,7 +25,7 @@ import com.synergyj.cursos.spring.service.TarjetaCreditoService;
 @RunWith(SpringJUnit4ClassRunner.class)
 // Probar con aopXmlSinNamespace.xml y después con aopXmlConNamespace.xml,
 // revisar archivos.
-// @ContextConfiguration("/aopXmlSinNamespace.xml")
+@ContextConfiguration("/aopXmlSinNamespace.xml")
 // @ContextConfiguration("/aopXmlConNamespace.xml")
 public class AopXMLTestCase {
 
